eee5a3109303fe32809a7e505ffd9aa4c21eb169
phuc <nguyendaihoangphuc24@gmail.com>
2024-06-06 15:18:09 +0700

    ChangeUi1

--- obj/Debug/net6.0/website_CLB_HTSV.AssemblyInfo.cs
+++ obj/Debug/net6.0/website_CLB_HTSV.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("website_CLB_HTSV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae4478477c1cd0282c8f103ea7be09153697160c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1d5f1bdd2089202f753df261f952649e43fb413")]
 [assembly: System.Reflection.AssemblyProductAttribute("website_CLB_HTSV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("website_CLB_HTSV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
